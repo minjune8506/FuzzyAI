fbdea0c24ff64a3268fca51e5f60f2847504b0f3
jaeyeon kim <jakind@naver.com>
2022-04-06 15:09:00 +0900

    [#1] 답장 오는 시간 getDegreesOfMembership 로 코드 간략화

--- function_answer.py
+++ function_answer.py
@@ -1,40 +1,14 @@
+from function_getDegreesOfMembership import getDegreesOfMembership
 # 답장이 오는데 걸리는 시간 (분)
 
 def low(answer):
-    c = 10
-    d = 40
-    if (answer < c) :
-        return 1
-    elif (c <= answer < d) :
-        return (d - answer) / (d - c)
-    else :
-        return 0
+    return getDegreesOfMembership(-1 , -1 , 10 , 40 , answer)
 
 def medium(answer) :
-    a = 10
-    b = 40
-    c = 60
-    d = 90
-    if (answer < a) :
-        return 0
-    elif (a <= answer < b) :
-        return (answer - a) / (b - a)
-    elif (b <= answer < c) :
-        return 1
-    elif (c <= answer < d) :
-        return (d - answer) / (d - c)
-    else :
-        return 0
+    return getDegreesOfMembership(10 , 40 , 60 , 90 , answer)
 
 def high(answer) :
-    a = 30
-    b = 120
-    if (answer < a) :
-        return 0
-    elif (a <= answer < b) :
-        return (answer - a) / (b - a)
-    else :
-        return 1
+    return getDegreesOfMembership(30 , 120 , -1 , -1 , answer)
 
 answer = int(input("답장이 오는데 걸리는 시간(분) : "))
 print("%.2f" %low(answer))
