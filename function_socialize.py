b0ded4c5f7d38af443c4c098347149f7d8c1a935
jaeyeon kim <jakind@naver.com>
2022-04-06 13:47:28 +0900

    [#1] function_socialize 함수  맘다니 형식으로 정의

--- function_socialize.py
+++ function_socialize.py
@@ -1,14 +1,50 @@
 def very_low(premise):
-    return premise * 10
+    c = 5
+    d = 20
+
+    if premise < c:
+        return 1
+    elif c <= premise < d:
+        return (d - premise) / (d - c)
+    else:
+        return 0
 
 def low(premise):
-    return premise * 30
+    return all_parameter_calculate(5 , 20 , 30 , 45 , premise)
 
-def middel(premise):
-    return premise * 50
+def middle(premise):
+    return all_parameter_calculate(30 , 45 , 55 , 70 , premise)
 
 def high(premise):
-    return premise * 70
+    return all_parameter_calculate(55 , 70 , 80 , 95 , premise)
 
 def very_high(premise):
-    return premise * 90
+    a = 80
+    b = 95
+
+    if premise < a:
+        return 0
+    elif a <= premise < b:
+        return (premise - a) / (b - a)
+    else:
+        return 1
+
+def all_parameter_calculate(a , b , c , d , premise):
+    if premise < a:
+        return 0
+    elif a <= premise < b:
+        return (premise - a) / (b - a)
+    elif b <= premise < c:
+        return 1
+    elif c <= premise < d:
+        return (d - premise) / (d - c)
+    else:
+        return 0
+
+premise = float(input())
+
+print("veryLow : " + str(very_low(premise)))
+print("low : " + str(low(premise)))
+print("middle : " + str(middle(premise)))
+print("high : " + str(high(premise)))
+print("veryhigh : " + str(very_high(premise)))
