d1bac2c79ad33316cebb1c32aa6d9a4ec48eb91e
SOLokill <shuai1593@gmail.com>
2022-04-06 18:15:01 +0900

    add new rule of meeting count

---
+++ function_count.py
@@ -0,0 +1,18 @@
+from function_getDegreesOfMembership import getDegreesOfMembership
+
+def low(count):
+    return getDegreesOfMembership(-1, -1, 5, 10, count)
+
+
+def medium(count):
+    return getDegreesOfMembership(5, 10, 20, 25, count)
+
+    
+def high(count):
+    return getDegreesOfMembership(20, 25, -1, -1, count)
+
+count = int(input("한달 동안 만나는 횟수(번)를 입력하시오 : "))
+
+print("적다고 볼 수 있는 확률 : " + str(low(count)))
+print("보통이라 볼 수 있는 확률 : " + str(medium(count)))
+print("많다고 볼 수 있는 확률 : " + str(high(count)))
