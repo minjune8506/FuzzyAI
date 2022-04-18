da96f2f5d9131712450cf3f0418f0aea1f91f125
jaeyeon kim <jakind@naver.com>
2022-04-18 16:14:24 +0900

    쓸데없는 파일 지우고 , 후건부 소속함수 총합 그래프로 표현 및 적분값 계산

--- rule_calculate.py
+++ rule_calculate.py
@@ -6,10 +6,19 @@ import func_distance
 import func_question
 import func_count as func_count
 import func_socialize
+import numpy
+import matplotlib.pyplot as plt
+very_low = 0
+low = 0
+middle = 0
+high = 0
+very_high = 0
 
 # Rule Number 1
 # 거리 low and 답장 low and 만남 low then 확률 medium
 def rule_1(distance , answer , count):
+    global middle    
+
     print("rule_1")
     print("distance : %d분, answer : %d분, count : %d번" %(distance, answer, count))
     print("거리 소속도 : %.1f" %func_distance.low(distance))
@@ -17,11 +26,14 @@ def rule_1(distance , answer , count):
     print("만남 소속도 : %.1f" %func_count.low(count))
     premise = min(func_distance.low(distance), func_answer.low(answer), func_count.low(count))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+
+    middle = max(middle , premise) 
 
 # Rule Number 2
 # 거리 high and 만남 low and 물음표 low then 확률 very low
 def rule_2(distance , count , question):
+    global very_low
+
     print("rule_2")
     print("distance : %d분, count : %d번, question : %d번" %(distance, answer, question))
     print("거리 소속도 : %.1f" %func_distance.high(distance))
@@ -29,11 +41,14 @@ def rule_2(distance , count , question):
     print("물음표 소속도 : %.1f" %func_question.low(question))
     premise = min(func_distance.high(distance), func_count.low(count), func_question.low(question))
     print("premise : %.1f" %premise)
-    return func_socialize.very_low(premise)
+
+    very_low = max(very_low , premise)
 
 # Rule Number 3
 # 거리 high and 만남 high and 답장 low then 확률 very high
 def rule_3(distance , count , answer):
+    global very_high
+
     print("rule_3")
     print("distance : %d분, answer : %d분, count : %d번" %(distance, answer, count))
     print("거리 소속도 : %.1f" %func_distance.low(distance))
@@ -41,11 +56,14 @@ def rule_3(distance , count , answer):
     print("답장 소속도 : %.1f" %func_answer.low(answer))
     premise = min(func_distance.low(distance), func_count.high(count), func_answer.low(answer))
     print("premise : %.1f" %premise)
-    return func_socialize.very_high(premise)
+
+    very_high = max(very_high , premise)
 
 # Rule Number 4
 # 거리 high and 만남 high and 답장 low and 물음표 high then 확률 very high
 def rule_4(distance , count , answer , question):
+    global very_high
+
     print("rule_4")
     print("distance : %d분, answer : %d분, count : %d번, question : %d번" %(distance, answer, count, question))
     print("거리 소속도 : %.1f" %func_distance.high(distance))
@@ -54,121 +72,154 @@ def rule_4(distance , count , answer , question):
     print("물음표 소속도 : %.1f" %func_question.high(question))
     premise = min(func_distance.high(distance), func_count.high(count), func_answer.low(answer), func_question.high(question))
     print("premise : %.1f" %premise)
-    return func_socialize.very_high(premise)
+
+    very_high = max(very_high , premise)
 
 # Rule Number 5
 # 거리 high and 만남 low then 확률 very low
 def rule_5(distance , count):
+    global very_low
+
     print("rule_5")
     print("distance : %d분, count : %d번" %(distance, count))
     print("거리 소속도 : %.1f" %func_distance.high(distance))
     print("만남 소속도 : %.1f" %func_count.low(count))
     premise = min(func_distance.high(distance) , func_count.low(count))
     print("premise : %.1f" %premise)
-    return func_socialize.very_low(premise)
+
+    very_low = max(very_low , premise)
 
 # Rule Number 6
 # 거리 medium and 만남 medium then 확률 medium
 def rule_6(distance, count):
+    global middle
+
     print("rule_6")
     print("distance : %d분, count : %d번" %(distance, count))
     print("거리 소속도 : %.1f" %func_distance.middle(distance))
     print("물음표 소속도 : %.1f" %func_count.middle(count))
     premise = min(func_distance.middle(distance), func_distance.middle(count))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+
+    middle = max(middle , premise)
 
 # Rule Number 7
 # 거리 high and 물음표 low then 확률 low
 def rule_7(distance, question) :
+    global low
+
     print("rule_7")
     print("distance : %d분, question : %d번" %(distance, question))
     print("거리 소속도 : %.1f" %func_distance.high(distance))
     print("물음표 소속도 : %.1f" %func_question.low(question))
     premise = min(func_distance.high(distance), func_question.low(question))
     print("premise : %.1f" %premise)
-    return func_socialize.low(premise)
+
+    low = max(low , premise)
 
 # Rule Number 8
 # 답장 low and 만남 medium then 확률 medium
 def rule_8(answer, count) :
+    global middle
+
     print("rule_8")
     print("answer : %d분, count : %d번" %(answer, count))
     print("답장 소속도 : %.1f" %(func_answer.low(answer)))
     print("만남 소속도 : %.1f" %(func_count.middle(count)))
     premise = min(func_answer.low(answer), func_count.middle(count))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+
+    middle = max(middle , premise)
 
 # Rule Number 9
 # 답장 high and 물음표 high then 확률은 medium
 def rule_9(answer, question) :
+    global middle
+
     print("rule_9")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %(func_answer.high(answer)))
     print("물음표 소속도 : %.1f" %(func_question.high(question)))
     premise = min(func_answer.high(answer), func_question.high(count))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+
+    middle = max(middle , premise)
 
 # Rule Number 10
 # 답장 high and 물음표 low then 확률 low.
 def rule_10(answer, question) :
+    global low
+
     print("rule_10")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %(func_answer.high(answer)))
     print("물음표 소속도 : %.1f" %(func_question.low(question)))
     premise = min(func_answer.high(answer), func_question.low(count))
     print("premise : %.1f" %premise)
-    return func_socialize.low(premise)
+    
+    low = max(low , premise)
 
 # Rule Number 11
 # 답장 high or 물음표 low then 확률 low.
 def rule_11(answer, question) :
+    global low
+
     print("rule_11")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %(func_answer.high(answer)))
     print("물음표 소속도 : %.1f" %(func_question.low(question)))
     premise = max(func_answer.high(answer), func_question.low(question))
     print("premise : %.1f" %premise)
-    return func_socialize.low(premise)
+
+    low = max(low , premise)
 
 # Rule Number 12
 # 답장 high and 물음표 high then 확률 high
 def rule_12(answer, question) :
+    global high
+
     print("rule_12")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %(func_answer.high(answer)))
     print("물음표 소속도 : %.1f" %(func_question.high(question)))
     premise = min(func_answer.high(answer), func_question.high(question))
     print("premise : %.1f" %premise)
-    return func_socialize.high(premise)
+    
+    high = max(high , premise)
 
 # Rule Number 13
 # 답장 low and 물음표 low then 확률 very low
 def rule_13(answer, question) :
+    global very_low
+
     print("rule_13")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %(func_answer.low(answer)))
     print("물음표 소속도 : %.1f" %(func_question.low(question)))
     premise = min(func_answer.low(answer), func_question.low(question))
     print("premise : %.1f" %premise)
-    return func_socialize.very_low(premise)
+
+    very_low = max(very_low , premise)
 
 # Rule Number 14
 # 답장 high and 만남 high then 확률 high
 def rule_14(answer, count) :
+    global high
+
     print("rule_14")
     print("answer : %d분, count : %d번" %(answer, count))
     print("답장 소속도 : %.1f" %(func_answer.high(answer)))
     print("만남 소속도 : %.1f" %(func_count.high(count)))
     premise = min(func_answer.high(answer), func_count.high(count))
     print("premise : %.1f" %premise)
-    return func_socialize.high(premise)
+
+    high = max(high , premise)
 
 # Rule Number 15
 # 답장 low and 만남 medium and 물음표 medium then 확률 medium
 def rule_15(answer, count, question):
+    global middle
+
     print("rule_15")
     print("answer : %d분, count : %d번, question : %d번" %(answer, count, question))
     print("답장 소속도 : %.1f" %func_answer.low(answer))
@@ -176,61 +227,77 @@ def rule_15(answer, count, question):
     print("물음표 소속도 : %.1f" %func_question.middle(question))
     premise = min(func_answer.low(answer), func_count.middle(count), func_question.middle(question))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+
+    middle = max(middle , premise)
 
 # Rule Number 16
 # 답장 medium and 물음표 low then 확률 low
 def rule_16(answer , question):
+    global low
+
     print("rule_16")
     print("answer : %d분, question : %d번" %(answer, question))
     print("답장 소속도 : %.1f" %func_answer.middle(answer))
     print("물음표 소속도 : %.1f" %func_question.low(question))
     premise = min(func_answer.middle(answer) , func_question.low(question))
     print("premise : %.1f" %premise)
-    return func_socialize.low(premise)
+    
+    low = max(low , premise)
 
 # Rule Number 17
 # 물음표 low or 만남 low then 확률 medium
 def rule_17(question, count):
+    global middle
+
     print("rule_17")
     print("question : %d번, count : %d번" %(question, count))
     print("물음표 소속도 : %.1f" %func_question.middle(question))
     print("만남 소속도 : %.1f" %func_count.low(count))
     premise = max(func_question.low(question), func_distance.low(count))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+    
+    middle = max(middle , premise)
 
 # Rule Number 18
 # 만남 high and 거리 high them 확률 high
 def rule_18(count, distance) :
+    global high
+
     print("rule_18")
     print("count : %d번, distance : %d분" %(count, distance))
     print("만남 소속도 : %.1f" %func_count.high(count))
     print("거리 소속도 : %.1f" %func_distance.high(distance))
     premise = min(func_count.high(count), func_distance.high(distance))
     print("premise : %.1f" %premise)
-    return func_socialize.high(premise)
+    
+    high = max(high , premise)
 
 # Rule Number 19
 # 만남 high or 물음표 high then 확률 medium
 def rule_19(count, question) :
+    global middle
+    
     print("rule_19")
     print("count : %d번, question : %d번" %(count, question))
     print("만남 소속도 : %.1f" %(func_count.high(count)))
     print("물음표 소속도 : %.1f" %(func_question.high(question)))
     premise = max(func_count.high(count), func_question.high(question))
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+    
+    middle = max(middle , premise)
 
 # Rule Number 20
 # 만남 low then 확률 medium
 def rule_20(count) :
+    global middle
+
     print("rule_20")
     print("count : %d번" %(count))
     print("만남 소속도 : %.1f" %(func_count.low(count)))
     premise = func_count.low(count)
     print("premise : %.1f" %premise)
-    return func_socialize.middle(premise)
+    
+    middle = max(middle , premise)
 
 #안 사귈수가 없어야 함
 distance = 15 # 거리 15분
@@ -258,3 +325,35 @@ print(rule_17(question,count))
 print(rule_18(count, distance))
 print(rule_19(count, question))
 print(rule_20(count))
+
+sum = 0
+
+very_low = 0.3
+low = 0.5
+middle = 0.7
+high = 0.9
+very_high = 1.0
+
+for i in numpy.arange(0.0 , 100.01 , 0.1):
+    # very_low 
+    vl = very_low if func_socialize.very_low(i) >= very_low else func_socialize.very_low(i)
+
+    # low
+    l = low if func_socialize.low(i) > low else func_socialize.low(i)
+
+    # middle
+    m = middle if func_socialize.middle(i) > middle else func_socialize.middle(i)
+
+    # high
+    h = high if func_socialize.high(i) > high else func_socialize.high(i)
+
+    # very_high
+    vh = very_high if func_socialize.very_high(i) > very_high else func_socialize.very_high(i)
+
+    # 그래프 그리기
+    plt.scatter(i , max(vl , l , m , h , vh) , c ='black' , edgecolor = 'black' , s = 10)
+    
+    sum += max(vl , l , m , h , vh) * 0.1
+
+plt.show() # 그래프 표시
+print(sum / 100) # 무게중심 값
